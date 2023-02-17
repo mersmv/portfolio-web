86b477683b6847e15238ffaf6da6a506ba1c19e7
mersmv <mercedes.sierra.miranda@gmail.com>
2023-02-17 14:57:16 +0100

    portfolio

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,37 @@
-
 import '../scss/styles.scss';
 //import { loquesea } from './dondesea.js';
+
+const menuClick = document.getElementById('menu');
+const menuShow = document.getElementById('menu-list');
+const leftIcon = document.getElementById('icon-left');
+const rightIcon = document.getElementById('icon-right');
+const presentationPhoto = document.getElementById('presentation-photo');
+const presentationText = document.getElementById('presentation-text');
+function callback(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      presentationPhoto.classList.add('appear-left');
+      presentationText.classList.add('appear-right');
+    } else {
+      presentationPhoto.classList.remove('appear-left');
+      presentationText.classList.remove('appear-right');
+    }
+  });
+}
+
+const options = {
+  root: null,
+  rootMargin: '-150px',
+  threshold: 0.5
+};
+
+const observer = new IntersectionObserver(callback, options);
+
+observer.observe(document.querySelector('.presentation__title'));
+
+menuClick.addEventListener('click', e => {
+  if (e.target.localName != 'p') return;
+  menuShow.classList.toggle('menu-show');
+  leftIcon.classList.toggle('translate-left');
+  rightIcon.classList.toggle('translate-right');
+});
